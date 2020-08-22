304add9ae85b77319c266073238d1a1d050dd640
csrawat <cs215100@gmail.com>
2020-08-22 23:26:08 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode.js');
 const forecast = require("./utils/forecast.js");
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 
 // app.com
 // app.com/help
@@ -87,6 +87,6 @@ app.get("*", (req,res) => {
   });
 })
 
-app.listen(3000,() => {
-    console.log("Server running on port 3000");
+app.listen(port,() => {
+    console.log("Server running on port " + port);
 })
